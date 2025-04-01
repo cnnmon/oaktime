360d17d8e686084bc47c7ceb34cb08c9774d7559
cnnmon <tiffanywang@berkeley.edu>
2025-03-31 19:10:40 -0700

    results

--- constants/results.js
+++ constants/results.js
@@ -1,16 +1,16 @@
 export const results = {
   helen: {
-    description: "heelen",
+    description: "kind, thoughtful, opinionated, meek, intentional, anxious, passionate, empathetic, awkward, reliable, observant, silly, honest - eric",
     image: "helen.jpeg",
     emote: "helen-emote.png",
   },
   tiffany: {
-    description: "you are opinionated and stand up for what you want when it matters. you are spontaneous, free-spirited, and happy to see your friends happy. you are good at coexisting with people, and there's no pressure to be someone i'm not, which i (helen) appreciate! and i like that you can comfortably have convos ranging from wildly chaotic rambles to serious convos abt hopes and dreams.",
+    description: "you are opinionated and stand up for what you want when it matters. you are spontaneous, free-spirited, and happy to see your friends happy. you are good at coexisting with people, and there's no pressure to be someone i'm not, which i appreciate! and i like that you can comfortably have convos ranging from wildly chaotic rambles to serious convos abt hopes and dreams. - helen",
     image: "tiffany.jpeg",
     emote: "tiffany-emote.png",
   },
   leyton: {
-    description: "you are a certified south bay boy. you are by far the most athletic and sporty in the hacker house. you are a gentle soul and can occasionally look high. you appreciate a good philosophical debate.",
+    description: "you are a certified south bay boy. you are by far the most athletic and sporty in the hacker house. you are a gentle soul and can occasionally look high. you appreciate a good philosophical debate. - tiffany",
     image: "leyton.jpeg",
     emote: "leyton-emote.png",
   },
